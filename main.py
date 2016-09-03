a471be438a62a92fe5482d8635d099329f3f2d43
Thomas Dang <thomas.kyle.dang@gmail.com>
2016-09-03 22:07:36 +0000

    Setup keyword recognition and bot listen module in anticipation of keyword plugin system

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import fbchat
-import yaml
+import fbchat, yaml
+
+class fbbot(fbchat.Client):
+    def __init__(self, email, password, debug=True, user_agent=None):
+        fbchat.Client.__init__(self, EMAIl, password, debug, user_agent) 
+
+    def on_message(self, mid, author_id, author_name, message, metadata):
+        self.markAsDelivered(author_id, mid)
+        self.markAsRead(author_id)
+
+        #Check for self author
+        if str(author_id) != str(self.uid):
+            #trigger word
+            if message.startswith("/bot"):
+                pass #TODO: Implement plugin system for commands here
 
 with open("config.yaml", "r") as stream:
     try:
@@ -9,4 +22,5 @@ with open("config.yaml", "r") as stream:
     except yaml.YAMLError as e:
         print(e)
 
-client = fbchat.Client(fb_user, fb_pass)
+client = fbbot(fb_user, fb_pass)
+client.listen()
